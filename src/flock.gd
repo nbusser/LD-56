f871d99a13727bdc696c8d7a38175e2a2ddd5c70
Louis MOUHAT <louis.mouhat@gmail.com>
2024-10-05 18:22:06 +0200

    update boids implementation

--- src/flock.gd
+++ src/flock.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 	for _i in range(30):
 		add_boid()
 
+var boids: Array[Boid] = []
+
 func add_boid() -> void:
 	var boid_instance = boid_scene.instantiate()
 	boid_instance.connect("painting_drop", Callable(painting, "on_painting_drop"))
@@ -18,17 +20,57 @@ func add_boid() -> void:
 
 	# TODO: handle that more properly
 	var random_offset = Vector2(
-		randf_range(10, 30),
-		randf_range(10, 30),
+		randf_range(0, 100),
+		randf_range(0, 100),
 	)
 	boid_instance.global_position += random_offset;
+	boid_instance.velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)) * 100
+	boids.append(boid_instance)
+
+var boids_center = Vector2()
+func _physics_process(delta):
+	boids_center = Vector2()
+	for boid in boids:
+		boids_center += boid.global_position
+	boids_center /= boids.size()
 
 func get_boids() -> Array[Boid]:
-	var boids: Array[Boid] = []
-	for node in boids_node.get_children():
-		if node is Boid:
-			boids.append(node as Boid)
 	return boids
+	# var boids: Array[Boid] = []
+	# for node in boids_node.get_children():
+	# 	if node is Boid:
+	# 		boids.append(node as Boid)
+	# return boids
+
+func get_visible_neighbours_center(boid: Boid) -> Vector2:
+	var neighbours = get_neighbour_boids(boid)
+	var center = Vector2.ZERO
+	var n = 0
+	for neighbour in neighbours:
+		if neighbour != boid and neighbour.global_position.distance_to(boid.global_position) > boid.repulsion_range:
+			center += neighbour.global_position
+			n += 1
+	if n > 0:
+		center /= n
+	return center
+	# var new_center = (boids_center * boids.size() - boid.global_position) / (boids.size() - 1)
+	# return new_center
+
+func get_repulsion_vector(boid: Boid) -> Vector2:
+	var repulsion_vector = Vector2()
+	for other_boid in get_neighbour_boids(boid):
+		var distance = other_boid.global_position.distance_to(boid.global_position)
+		if distance < boid.repulsion_range:
+			repulsion_vector += (boid.global_position - other_boid.global_position) / pow(distance, 2)
+	return repulsion_vector
+
+func get_alignment_vector(boid: Boid) -> Vector2:
+	var alignment_vector = Vector2()
+	for other_boid in get_neighbour_boids(boid):
+		alignment_vector += other_boid.velocity
+	if get_neighbour_boids(boid).size() > 0:
+		alignment_vector /= get_neighbour_boids(boid).size()
+	return alignment_vector
 
 func get_neighbour_boids(boid: Boid) -> Array[Boid]:
 	return get_boids().filter(
