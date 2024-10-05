251f7ce2af39f2ad51719bbc8fc39435998774db
caimez323 <caimezae@gmail.com>
2024-10-05 17:12:12 +0200

    add a function to calcul the similarity between 2 images

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -1,11 +1,34 @@
 extends Node2D
 
-var model_array = []
+var model_image : Image
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#print(compare_two_images(model_image,model_image))
 	pass	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# This fonction compare two images and return a value
+func compare_two_images(image1 : Image, image2 : Image) -> int :
+	var DIFFERENCE_PRECISION = 100
+	image1.resize(min(image1.get_width(),image2.get_width()),min(image1.get_height(),image2.get_height()))
+	image2.resize(min(image1.get_width(),image2.get_width()),min(image1.get_height(),image2.get_height()))
+	var distancePbP : Array
+	for y in range(image1.get_height()):
+		for x in range(image1.get_width()):
+			#Run distance_squared cause it is faster (source:godot doc)
+			var pixelColor1 = image1.get_pixel(x,y)
+			var pixelVector1 : Vector3 = Vector3(pixelColor1.r*DIFFERENCE_PRECISION,pixelColor1.g*DIFFERENCE_PRECISION,pixelColor1.b*DIFFERENCE_PRECISION)
+			var pixelColor2 = image1.get_pixel(x,y)
+			var pixelVector2 : Vector3 = Vector3(pixelColor2.r*DIFFERENCE_PRECISION,pixelColor2.g*DIFFERENCE_PRECISION,pixelColor2.b*DIFFERENCE_PRECISION)
+			distancePbP.append(pixelVector1.distance_squared_to(pixelVector2))
+	var sum : int
+	for i in distancePbP : sum+= i
+	#Plus la note est éloignée de 0, moins c'est bien
+	#La pire note c'est 3*DIFFERENCE_PRECISION**2
+	return sum/len(distancePbP)
+	
--- src/Model/Model.gd
+++ src/Model/Model.gd
@@ -3,17 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var texture = $Area2D/Sprite2D.texture as CompressedTexture2D
-	var image = texture.get_image()
-	var pixel_array = []
-	for y in range(image.get_height()):
-		var px_row = [] 
-		for x in range(image.get_width()):
-			var color = image.get_pixel(x, y)
-			px_row.append(color)
-		pixel_array.append(px_row)
+	#Transfer right now the image to the map 
 	var map_node = self.get_parent()
-	map_node.model_array = pixel_array
+	var texture = $Area2D/Sprite2D.texture as CompressedTexture2D
+	map_node.model_image = texture.get_image()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
