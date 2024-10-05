ba8fc02ec58a7fff2d15f9454bba2fb91f59036d
caimez323 <caimezae@gmail.com>
2024-10-05 17:28:45 +0200

    change image compare from int to float

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -13,22 +13,20 @@ func _process(delta: float) -> void:
 
 
 # This fonction compare two images and return a value
-func compare_two_images(image1 : Image, image2 : Image) -> int :
-	var DIFFERENCE_PRECISION = 100
+func compare_two_images(image1 : Image, image2 : Image) -> float :
 	image1.resize(min(image1.get_width(),image2.get_width()),min(image1.get_height(),image2.get_height()))
 	image2.resize(min(image1.get_width(),image2.get_width()),min(image1.get_height(),image2.get_height()))
-	var distancePbP : Array
+	var sum : float = 0.0
 	for y in range(image1.get_height()):
 		for x in range(image1.get_width()):
 			#Run distance_squared cause it is faster (source:godot doc)
 			var pixelColor1 = image1.get_pixel(x,y)
-			var pixelVector1 : Vector3 = Vector3(pixelColor1.r*DIFFERENCE_PRECISION,pixelColor1.g*DIFFERENCE_PRECISION,pixelColor1.b*DIFFERENCE_PRECISION)
-			var pixelColor2 = image1.get_pixel(x,y)
-			var pixelVector2 : Vector3 = Vector3(pixelColor2.r*DIFFERENCE_PRECISION,pixelColor2.g*DIFFERENCE_PRECISION,pixelColor2.b*DIFFERENCE_PRECISION)
-			distancePbP.append(pixelVector1.distance_squared_to(pixelVector2))
-	var sum : int
-	for i in distancePbP : sum+= i
+			var pixelVector1 = Vector3(pixelColor1.r,pixelColor1.g,pixelColor1.b)
+			var pixelColor2 = image2.get_pixel(x,y)
+			var pixelVector2 = Vector3(pixelColor2.r,pixelColor2.g,pixelColor2.b)
+			sum += pixelVector1.distance_squared_to(pixelVector2)
+	
 	#Plus la note est éloignée de 0, moins c'est bien
-	#La pire note c'est 3*DIFFERENCE_PRECISION**2
-	return sum/len(distancePbP)
+	#On divise par 3 pour normaliser en 3D
+	return sum/(3*(image1.get_width()*image1.get_height()))
 	
