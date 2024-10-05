4ef01a7a8aab5e0de194b0e83d376ab67957acb1
Nicolas Busser <nicolas.busser@lacollection.io>
2024-10-05 22:47:43 +0900

    optims

--- src/Painting/painting.gd
+++ src/Painting/painting.gd
@@ -14,10 +14,9 @@ var image: Image = Image.create(CANVAS_SIZE, CANVAS_SIZE, false, Image.FORMAT_RG
 func _ready():
 	self.image.fill(Color.WHITE)
 	self.texture = ImageTexture.create_from_image(self.image)
-	queue_redraw()
 
 func _process(_delta):
-	pass
+	self.texture.update(self.image);
 
 
 func to_pack_vector2_array(array: Array[Vector2]) -> PackedVector2Array:
@@ -27,5 +26,3 @@ func to_pack_vector2_array(array: Array[Vector2]) -> PackedVector2Array:
 # TODO: eventually add linear velocity to draw a quick line
 func on_painting_drop(boid_position: Vector2, color: Color) -> void:
 	self.image.set_pixelv(boid_position, color);
-	self.texture.update(self.image);
-	queue_redraw()
