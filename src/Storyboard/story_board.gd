017492e9e41504189cea0c439f7aef18563f3d84
Nicolas Busser <nicolas.busser@lacollection.io>
2024-10-10 21:21:58 +0900

    add cutscene skip button

--- src/Storyboard/story_board.gd
+++ src/Storyboard/story_board.gd
@@ -5,10 +5,18 @@ extends Control
 signal start_game
 
 func _ready():
+	$Skip.modulate = Color.TRANSPARENT
+	
 	animated.play('default')
 	animated.connect('animation_finished', self._on_animation_finished)
 
+	await get_tree().create_timer(2.5).timeout
+	create_tween().tween_property($Skip, "modulate", Color.WHITE, 2).finished
+	
 func _on_animation_finished():
-	print('finished')
+	emit_signal("start_game")
+	queue_free()
+
+func _on_skip_pressed() -> void:
 	emit_signal("start_game")
 	queue_free()
