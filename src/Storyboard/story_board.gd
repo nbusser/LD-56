2f887dd5bc0efc65fb1a06514551f53c0c4c74a9
Antoine Brûlé <antoine.brule@netc.eu>
2024-10-12 19:03:43 +0200

    intro: add fading transitions

--- src/Storyboard/story_board.gd
+++ src/Storyboard/story_board.gd
@@ -1,19 +1,30 @@
 extends Control
 
-@onready var animated = $AnimatedSprite2D
+@onready var animated: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated2: AnimatedSprite2D = $AnimatedSprite2D2
+@onready var black_rect: ColorRect = $BlackRect
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 signal storyboard_finished
 
 func _ready():
 	$Skip.modulate = Color.TRANSPARENT
+	black_rect.show()
+	black_rect.modulate = Color.WHITE
+	await create_tween().tween_property(black_rect, "modulate", Color.TRANSPARENT, 2).finished
+	animation_player.play("animated_sprite_opacity")
 	
+	animated2.play('default')
+	await get_tree().create_timer(1).timeout
 	animated.play('default')
+	
 	animated.connect('animation_finished', self._on_animation_finished)
 
 	await get_tree().create_timer(2.5).timeout
-	create_tween().tween_property($Skip, "modulate", Color.WHITE, 2).finished
+	create_tween().tween_property($Skip, "modulate", Color.WHITE, 2)
 	
 func _on_animation_finished():
+	await create_tween().tween_property(black_rect, "modulate", Color.WHITE, 2).finished
 	emit_signal("storyboard_finished")
 	queue_free()
 
